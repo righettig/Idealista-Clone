15e8263c66b1203549b53e7a02a7aee3bbf4bc52
Giacomo Righetti <kirkrulez83@gmail.com>
2024-07-08 13:13:17 +0200

    Updated bootstrap version

--- idealista-react/pages/_document.js
+++ idealista-react/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
         />
         <link rel="stylesheet" href="css/globals.css" />
